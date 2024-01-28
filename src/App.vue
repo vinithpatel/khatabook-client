a928b02066f4085cdb533b2b7d2f1e9b75595df7
Vinith <viniththammala12@gmail.com>
2024-01-28 21:07:13 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class= "logo" alt="Vue logo" src="https://img.freepik.com/free-vector/electronic-documentation-man-with-registration-checking-repository-log-online-approval-screen-form-validation-page-expense-chronicles-vector-isolated-concept-metaphor-illustration_335657-4323.jpg?t=st=1706447604~exp=1706448204~hmac=fde5065ccfc4861836e7764466952b45153342af057c7ef036177f7d1066c86b">
+  <HelloWorld msg="Welcome to Khatabook"/>
 </template>
 
 <script>
@@ -22,5 +22,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo{
+  height: 300px;
 }
 </style>
