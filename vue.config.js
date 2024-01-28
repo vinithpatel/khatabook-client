9cb6101d3b490119cda21fed882d6f8970c59e86
Vinith <viniththammala12@gmail.com>
2024-01-28 21:36:30 +0530

    vue.config.js update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/khatabook-client/'  // Change this to your repository name
+    : '/',
+
   transpileDependencies: true
 })
